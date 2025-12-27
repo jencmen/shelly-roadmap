5c4004d32c8011a5e629ba490483fd23e1a4481a
jencmen <jencmen@gmail.com>
2025-12-27 19:43:09 +0200

    Correction to Vite Config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // This must match your repository name on GitHub
   base: '/shelly-roadmap/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      // Ensure we can resolve files from the root
+      '@': '/',
+    },
   },
 });
